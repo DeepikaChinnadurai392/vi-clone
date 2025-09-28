270e3da1dc4921b814b68dbff9446eddbc88db8f
DEEPIKA C <cdeepikad08@gmail.com>
2025-09-28 18:52:50 +0530

    Delete frontend directory

--- frontend/signup.js
+++
@@ -1,8 +0,0 @@
-export default function SignupPage() {
-  return (
-    <div className="signup-container">
-      <h2>Signup Page</h2>
-      <p>This is where the signup form will go.</p>
-    </div>
-  );
-}
